d8f4a945b814c8aa016e2d342b7ef5331113506e
hacxy <hacxy.js@outlook.com>
2025-11-17 14:45:26 +0800

    Update database adapter from Better SQLite3 to MariaDB; modify Prisma schema and client initialization accordingly, ensuring compatibility with new database provider.

--- src/common/prisima.ts
+++ src/common/prisima.ts
@@ -1,8 +1,14 @@
 import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaError } from "@/common/errors";
-import { PrismaBetterSQLite3 } from "@prisma/adapter-better-sqlite3";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 
-const adapter = new PrismaBetterSQLite3(process.env.DATABASE_URL);
+const adapter = new PrismaMariaDb({
+  host: process.env.DATABASE_HOST,
+  port: parseInt(process.env.DATABASE_PORT || "3306"),
+  user: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+});
 const basePrisma = new PrismaClient({ adapter });
 
 export const prisma = basePrisma.$extends({
