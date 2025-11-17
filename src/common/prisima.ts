e412f3a700a3543610ea2d196bd572878627109a
hacxy <hacxy.js@outlook.com>
2025-11-17 11:04:04 +0800

    Add Prisma Better SQLite3 adapter and update dependencies in package.json and bun.lock; refactor Prisma client initialization in prisima.ts

--- src/common/prisima.ts
+++ src/common/prisima.ts
@@ -1,7 +1,9 @@
 import { PrismaClient } from "@/generated/prisma/client";
 import { PrismaError } from "@/common/errors";
+import { PrismaBetterSQLite3 } from "@prisma/adapter-better-sqlite3";
 
-const basePrisma = new PrismaClient();
+const adapter = new PrismaBetterSQLite3(process.env.DATABASE_URL);
+const basePrisma = new PrismaClient({ adapter });
 
 export const prisma = basePrisma.$extends({
   query: {
